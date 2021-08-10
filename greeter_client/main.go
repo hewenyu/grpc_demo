1ea446aa7fdc2b357995e7df57d2b0d2d839de88
yueban <yuebanlaosiji@outlook.com>
2021-08-10 09:28:31 +0800

    rpc

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -55,4 +55,11 @@ func main() {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
+	//  new
+
+	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
 }
